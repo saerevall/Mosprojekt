803811c48caf480e91b79beb23427b1098395acb
Cricka <crisa902@student.liu.se>
2016-03-01 11:06:00 +0100

    add movement to the camera

--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -3,21 +3,29 @@ package com.mygdx.game;
 /**
  * Created by Cricka on 2/23/2016.
  */
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 public class WorldRenderer {
 
+    static final float FRUSTUM_WIDTH = 10;
+    static final float FRUSTUM_HEIGHT = 15;
     WorldUpdate update;
     SpriteBatch batch;
+    OrthographicCamera cam;
     public WorldRenderer(WorldUpdate update, SpriteBatch batch){
         this.update = update;
         this.batch = batch;
+        this.cam = new OrthographicCamera(FRUSTUM_WIDTH, FRUSTUM_HEIGHT);
+        this.cam.position.set(FRUSTUM_WIDTH / 2, FRUSTUM_HEIGHT / 2, 0);
     }
 
     public void render(){
 
         renderBackground();
         renderObjects();
+        cam.update();
+        batch.setProjectionMatrix(cam.combined);
 
     }
 
