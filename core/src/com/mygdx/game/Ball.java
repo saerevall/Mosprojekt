58ab4fba8dc2a20d34472785b9ed1c7ad8afdb01
Cricka <crisa902@student.liu.se>
2016-03-01 11:47:51 +0100

    added DynamicGameObject.java

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -7,19 +7,19 @@ package com.mygdx.game;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Circle;
 
-public class Ball extends GameObject {
+public class Ball extends DynamicGameObject {
 
-    //public final Vector2 vel;
+
     //public final Vector2 accel;
     public final Circle bounds;
     public static final float RADIUS = 2;
 
 
 
-    public Ball(float x, float y){
+    public Ball(float posx, float posy, float velx, float vely){
 
-            super(x,y);
-            this.bounds = new Circle(x, y, RADIUS);
+            super(posx, posy, velx, vely);
+            this.bounds = new Circle(posx, posy, RADIUS);
 
     }
     public void update(float dt){
@@ -28,6 +28,7 @@ public class Ball extends GameObject {
 
     public void setPos(Vector2 pos) {
         position = pos;
+        bounds.setPosition(position);
     }
 
     public Vector2 getPos(){
