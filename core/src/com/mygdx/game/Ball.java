82a6d78f3f02fa6597e0fcb95a27a0005a778f3f
Love <lovli064@student.liu.se>
2016-03-01 11:48:49 +0100

    a

--- core/src/com/mygdx/game/Ball.java
+++ core/src/com/mygdx/game/Ball.java
@@ -7,24 +7,13 @@ package com.mygdx.game;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Circle;
 
-<<<<<<< HEAD
 
-public class Ball {
-
-    public Vector2 vel;
-    public Vector2 acc;
-    public Vector2 pos;
-    public final Circle bounds;
-    public Ball(float x, float y, float rad){
-
-            this.bounds = new Circle(x, y, rad);
-            this.acc = new Vector2(0, 0);
-            this.vel = new Vector2(0,0);
 
 public class Ball extends GameObject {
 
-    //public final Vector2 vel;
-    //public final Vector2 accel;
+    public Vector2 velocity;
+    public Vector2 acceleration;
+
     public final Circle bounds;
     public static final float RADIUS = 2;
 
@@ -39,8 +28,8 @@ public class Ball extends GameObject {
     }
     public void update(float dt){
 
-        vel = new Vector2((acc.x + WorldUpdate.gravity.x)*dt, (acc.y + WorldUpdate.gravity.y)*dt);
-        pos = new Vector2(vel.x * dt, vel.y * dt);
+        velocity = new Vector2((acceleration.x + WorldUpdate.gravity.x)*dt, (acceleration.y + WorldUpdate.gravity.y)*dt);
+        position = new Vector2(velocity.x * dt, velocity.y * dt);
 
 
     }
@@ -59,8 +48,7 @@ public class Ball extends GameObject {
 //    }
 
     public Vector2 getPos(){
-
-
+        
         return position;
 
     }
